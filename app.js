f8353aeeac435f32212239cab54bcdcd94e37674
jjsub <jsamesub@gmail.com>
2015-03-13 14:11:53 -0500

    playing with differente services

--- app.js
+++ app.js
@@ -2,22 +2,43 @@
 var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
-angularApp.controller('mainController', function ($scope) {
+angularApp.controller('mainController', function ($scope, $log, $filter) {
   
 
-  $scope.name       = "juan";
-  $scope.occupation = "programmer";
-  $scope.getName    = function(){
-      return "Inge deRodriguez";
-  };
-  $scope.bla = $scope.getName();
-  console.log($scope);
-  console.log($scope.getName());
+  $scope.name = "Juan";
+  $scope.formattedname = $filter('uppercase')($scope.name);
+
+  $log.info($scope.name);
+  $log.info($scope.formattedname);
+
+// console.log($log);
+
+// $log.log("Hooola!!!");
+// $log.info("This is some greate information about angular.js");
+// $log.warn("Warning!!!!!");
+// $log.debug("Some debug info");
+// $log.error("Errrorrrrrr!!!!");
 
 });
 
-var searchPeople = function(firstName, lastName, height, age, occupation){
-    return "Wil Palomo";
- };
 
-console.log(angular.injector().annotate(searchPeople));
+
+
+
+
+//   $scope.name       = "juan";
+//   $scope.occupation = "programmer";
+//   $scope.getName    = function(){
+//       return "Inge deRodriguez";
+//   };
+//   $scope.bla = $scope.getName();
+//   console.log($scope);
+//   console.log($scope.getName());
+
+// });
+
+// var searchPeople = function(firstName, lastName, height, age, occupation){
+//     return "Wil Palomo";
+
+
+// console.log(angular.injector().annotate(searchPeople));
